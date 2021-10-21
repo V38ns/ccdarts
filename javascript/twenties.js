a63bd95837e7937a230d638d0c650f54a7c7da4b
V38ns <91935083+V38ns@users.noreply.github.com>
2021-10-21 16:17:41 +0200

    Add files via upload

---
+++ javascript/twenties.js
@@ -0,0 +1,193 @@
+var clickspu = document.getElementById("punten");
+    var clickspo = document.getElementById("pogingen");
+    function plus1() {
+        clickspu += 1;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus2() {
+        clickspu += 2;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus3() {
+        clickspu += 3;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus4() {
+        clickspu += 4;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus5() {
+        clickspu += 5;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus6() {
+        clickspu += 6;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus7() {
+        clickspu += 7;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus8() {
+        clickspu += 8;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function plus9() {
+        clickspu += 9;
+        clickspo += 1;
+        document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min1() {
+        clickspu -= 1;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min2() {
+        clickspu -= 2;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min3() {
+        clickspu -= 3;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min4() {
+        clickspu -= 4;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min5() {
+        clickspu -= 5;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min6() {
+        clickspu -= 6;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min7() {
+        clickspu -= 7;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min8() {
+        clickspu -= 8;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function min9() {
+        clickspu -= 9;
+        clickspo += 1;
+        if (clickspu < 0) {
+            document.getElementById("punten").innerHTML = 0;
+            clickspu = 0;
+            }
+        if (clickspu >= 0) document.getElementById("punten").innerHTML = clickspu;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function zero() {
+        clickspo += 1;
+        document.getElementById("pogingen").innerHTML = clickspo;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
+    function leegmaken() {
+        document.getElementById("punten").innerHTML = 0;
+        document.getElementById("pogingen").innerHTML = 0;
+        document.getElementById("puntentxt").value= document.getElementById("punten").innerHTML;
+        document.getElementById("pogingentxt").value= document.getElementById("pogingen").innerHTML;
+    };
